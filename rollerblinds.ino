4abac4dedc712a22f311fb8e699a9182d4f57bd9
deerhunter <fallecimiento@gmail.com>
2017-03-24 15:37:47 +0300

    Update rollerblinds.ino

--- rollerblinds.ino
+++ rollerblinds.ino
@@ -1,7 +1,7 @@
 #include <Stepper.h>
 
 //pins
-#define BUTTON_PIN 13
+#define BUTTON_PIN 13 //Set correct pins for stuff
 #define TEMP_PIN 12
 #define LIGHT_PIN 14
 #define LIGHT_SENSOR1_PIN 15
@@ -13,6 +13,14 @@ int calibrated=0; //1- calibrating down 2- calibtaring up 3 - calibrated
 int state = 3; //1-up 2-down 3-middle(unknown)
 int stepsMAX=0; // maximum steps for full travel
 
+// Sensors variables
+int light_out=0;
+int light_in=0;
+int soil_humid=0;
+int temperature=0;
+int humidity=0;
+
+
 //additional variables
 #define BUTTON_TRESHOLD 150
 unsigned long BUTTON_TIME=0;
@@ -22,6 +30,7 @@ unsigned long time_end;
 //Stepper
 const int stepsPerRevolution = 4096;  // 64 steps per revolution and 1/64 gear is 64*64=4096 steps per revolution
 Stepper stepper1(stepsPerRevolution,5,0,2,4);
+stepper.setSpeed(10); // may need to move it somewhere
 int direction = 1; //Change to -1 if stepper is going in wrong direction
 
 //WI-FI
@@ -37,8 +46,7 @@ void setup()
 void calibration()
 { 
 Serial.println("Starting calibration");
-//do calibration
-while(digitalRead(BUTTON_PIN) == LOW) { };
+while(digitalRead(BUTTON_PIN) == LOW) { }; //wait for button press to start calibration
   
 time_start = millis();
 while(digitalRead(BUTTON_PIN) == HIGH) { };
@@ -46,31 +54,35 @@ time_end = millis();
 BUTTON_TIME=time_end-time_start;
   
 if (BUTTON_TIME>=2000){ //closes at the end of sub
+
  calibrated=1; //calibrating low position
  Serial.println("Going down");
-
-  while(digitalRead(BUTTON_PIN) == LOW) {
-//go down untill button is pressed again
-    stepper1.step(-1*direction);
+while(digitalRead(BUTTON_PIN) == LOW) {
+ stepper1.step(-1*direction);
 }
+state = 2; // Note that we are in bottom position
 calibrated=2; //calibrating high position
 Serial.println("Going up");
 
-while(digitalRead(BUTTON_PIN) == LOW) {
-//go up untill button is pressed again and count steps
+while(digitalRead(BUTTON_PIN) == LOW) { //go up untill button is pressed again and count steps
   stepper1.step(1*direction);
   stepsMAX++; //steps counted in run up
  }
  calibrated=3;
+ state = 1; // Note that we are in top position
  Serial.println("Calibration complete"); 
+ Serial.print("Maximum steps for one run is ");
+ Serial.println(stepsMAX); 
 }
 }
 
 void loop()
 { 
-
-if (calibrated==0){
+if (calibrated==0){ //Check if we are calibrated
+Serial.println("Requires calibration"); 
 calibration();
+Serial.println("Returned after calibration");
+Serial.println("Blinds are full open now");
 }
 
 //do something
